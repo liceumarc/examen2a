dd387c20df419ee8ba546153183244e8bd00c1cb
liceumarc <mmorellw@esliceu.net>
2025-04-10 20:54:15 +0200

    examen

--- js/index.js
+++ js/index.js
@@ -15,14 +15,22 @@ btnCalculaArea.onclick = calculaArea;
 btnNetejaArea.onclick = netejaArea;
 
 // FUNCIONS Exercici 1
+
 function calculaArea() {
+
+    if(parseInt(inputRadi.value) < MIN_RADI || parseInt(inputRadi.value) > MAX_RADI){
+        alert("El número no es válido");
+    }
+
+    let calculaArea = PI * Math.pow(parseInt(inputRadi.value), 2);
+    
+    spanAreaResult.textContent=calculaArea;
 
 }
 
 function netejaArea() {
-
+    spanAreaResult.textContent="";
 }
-
 
 //EXERCICI 2 ----------------------------------------------
 // CONSTANTS I VARIABLES GLOBALS - Exercici 2: Casino - Ruleta
@@ -40,11 +48,44 @@ btnNetejaRuleta.onclick = netejaRuleta;
 
 // FUNCIONS Exercici 2
 function generaNumRuleta() {
+    return Math.round(Math.random()* 35);
+}
 
+function actualizarNumero(){
+    numeroActual = generaNumRuleta();
+    ruletaDisplay.textContent = numeroActual;
+}
+
+let ruletaGirando = false;
+let interval = null;
+
+function iniciar(){
+    ruletaGirando = true;
+    btnGiraRuleta.textContent = "Parar";
+    actualizarNumero();
+    interval = setInterval(actualizarNumero, 500);
+}
+
+function parar(){
+    clearInterval(interval);
+    ruletaGirando = false;
+    btnGiraRuleta.textContent = "Iniciar";
+    if(parseInt(numeroActual) % 2 == 0){
+        ruletaDisplay.style.backgroundColor = "red";
+    } else if(numeroActual % 2 !== 0 && !0){
+        ruletaDisplay.style.backgroundColor = "black";
+    } else {
+        ruletaDisplay.style.backgroundColor = "green";
+    }
+    historialRuleta.textContent += `Has apostat: ${numeroActual}\n`
 }
 
 function giraRuleta() {
-
+    if(ruletaGirando){
+        parar();
+    } else {
+        iniciar();
+    }
 }
 
 function netejaRuleta() {
@@ -67,7 +108,20 @@ btnNetejaJson.onclick = netejaJson;
 
 // FUNCIONS Exercici 3
 function carregaDades() {
- 
+    fetch('./json/brainrot.json').then(response => {
+        if (response.ok){
+            return response.json();
+        } else {
+            throw new Error('Error al cargar el archivo JSON');
+        }
+    }).then(data => {
+
+        data.forEach(usuario => {
+            const select = document.createElement("option");
+            select.value = `${usuario}`;
+            selectLlistat.textContent = select;
+        })
+    })
 }
 
 function mostraDetall() {
